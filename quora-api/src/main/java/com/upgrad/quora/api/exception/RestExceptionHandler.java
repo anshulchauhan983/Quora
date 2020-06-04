ec7fdb815a8e63e130ca11a157ccf57ba7881093
anshulchauhan983 <anshulchauhan983@gmail.com>
2020-06-04 11:17:17 +0530

    endpoint-3-full

--- quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
+++ quora-api/src/main/java/com/upgrad/quora/api/exception/RestExceptionHandler.java
@@ -52,4 +52,11 @@ public class RestExceptionHandler {
                 new ErrorResponse().code(exc.getCode()).message(exc.getErrorMessage()), HttpStatus.NOT_FOUND
         );
     }
+
+    @ExceptionHandler(AnswerNotFoundException.class)
+    public ResponseEntity<ErrorResponse> answerNotFoundException(AnswerNotFoundException exc, WebRequest request){
+        return new ResponseEntity<ErrorResponse>(
+                new ErrorResponse().code(exc.getCode()).message(exc.getErrorMessage()),HttpStatus.NOT_FOUND
+        );
+    }
 }
---
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -0,0 +1,48 @@
+package com.upgrad.quora.service.dao;
+
+import com.upgrad.quora.service.entity.Answer;
+import com.upgrad.quora.service.entity.Questions;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class AnswerDao {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Answer createAnswer(Answer answer) {
+        entityManager.persist(answer);
+        return answer;
+    }
+    public List<Answer> getAllAnswer() {
+        return entityManager.createQuery("SELECT a FROM Answer a", Answer.class)
+                .getResultList();
+    }
+    public Answer editAnswer(final Answer answerEntity) {
+        return entityManager.merge(answerEntity);
+    }
+    public Answer getAnswerByUuid(final String AnswerByUuid) {
+        try {
+            return entityManager.createNamedQuery("AnswerByUuid", Answer.class)
+                    .setParameter("uuid", AnswerByUuid).getSingleResult();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+    public Answer deleteAnswer(final Answer answerEntity) {
+        entityManager.remove(answerEntity);
+        return answerEntity;
+    }
+    public List<Answer> getAnswersByQuestion(final Questions question){
+        try {
+            return entityManager.createNamedQuery("getAnswersByQuestion", Answer.class)
+                    .setParameter("question",question ).getResultList();
+        } catch (NoResultException nre) {
+            return null;
+        }
+    }
+}
